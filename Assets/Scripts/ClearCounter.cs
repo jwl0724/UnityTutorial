1e1ee5893a4f97d029e2a2281bcd668cbbf8f8bf
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-02-04 04:55:31 -0800

    add picking up items

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,24 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        
+        if (!HasKitchenObject()) {
+            // No KitchenObject here
+            if (player.HasKitchenObject()) {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                // Player not carrying anything
+
+            }
+        } else {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject()) {
+                // Player is carrying something
+
+            } else {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
