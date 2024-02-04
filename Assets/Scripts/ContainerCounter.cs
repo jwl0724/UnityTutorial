d0d1153de8b0336b27169937242674c9028c8ef2
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-02-04 06:14:54 -0800

    add static method for spawning kitchObjects

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -14,8 +14,7 @@ public class ContainerCounter : BaseCounter {
     public override void Interact(Player player) {
         if (!player.HasKitchenObject()) {
             // Player not carrying anything
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
 
             OnPlayerGrabbedObject?.Invoke(this, AssemblyLoadEventArgs.Empty);
         }
