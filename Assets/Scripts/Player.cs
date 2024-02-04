b3da341dc826f44733d183731d524589648f8827
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-02-04 05:01:46 -0800

    fix visual bug for rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -92,7 +92,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove) {
                 // can move only on the X axis
@@ -102,7 +102,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
                 // attempt Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove) {
                     // can move only on the Z
