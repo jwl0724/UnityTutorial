92e60a75be3f78a2ffbd620c2870c72ff6767815
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-02-12 04:08:46 -0800

    add plate interaction with counter logic

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -21,7 +21,20 @@ public class ClearCounter : BaseCounter {
             // There is a KitchenObject here
             if (player.HasKitchenObject()) {
                 // Player is carrying something
-
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                } else {
+                    // player not carrying plate but something else
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject)) {
+                        // counter is holding a plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             } else {
                 // Player is not carrying anything
                 GetKitchenObject().SetKitchenObjectParent(player);
